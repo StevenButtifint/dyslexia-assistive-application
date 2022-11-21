20a8da5aa01cb8e4863e97b288e874ad860e9d41
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-21 12:10:28 +0000

    added make label function

--- res/interface.py
+++ res/interface.py
@@ -5,3 +5,10 @@ def make_static_frame(frame, bg, w, h, x, y, anchor):
     return new_frame
 
 
+def make_label(frame, text, bg, fg, x, y, anchor, size):
+    label = tk.Label(frame, text=text, bg=bg, fg=fg)
+    label.place(x=x, y=y, anchor=anchor)
+    label['font'] = font.Font(family='Helvetica', size=size)
+    return label
+
+
