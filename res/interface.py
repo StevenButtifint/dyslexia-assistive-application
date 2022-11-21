0a712ec1db1c8c9f1c5a50a0b6751ebbeee1251f
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-21 12:00:09 +0000

    added make static frame function

--- res/interface.py
+++ res/interface.py
@@ -1,1 +1,7 @@
 import tkinter as tk
+def make_static_frame(frame, bg, w, h, x, y, anchor):
+    new_frame = tk.Frame(frame, bg=bg)
+    new_frame.place(width=w, height=h, x=x, y=y, anchor=anchor)
+    return new_frame
+
+
