5d3964f15a0515570bc48c69b886ca140f585c5b
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-22 10:41:00 +0000

    added toolbar frame

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ class Window:
         self.window.geometry(HOME_DIMENSIONS)
         self.window.title(APP_TITLE)
         self.window.iconbitmap(self.resource_path(APP_ICON_DIR))
+        self._create_tool_bar()
+
+    def _create_tool_bar(self):
+        toolbar_frame = make_static_frame(self.window, TOOLBAR_BG, 2000, 85, 0, 0, "nw")
 
 
 if __name__ == "__main__":
