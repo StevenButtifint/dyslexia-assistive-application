97b0b90c4acdd6291d1fbd5436d0ace4ef78d15f
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-22 10:41:41 +0000

    added make tool button

--- res/interface.py
+++ res/interface.py
@@ -12,3 +12,21 @@ def make_label(frame, text, bg, fg, x, y, anchor, size):
     return label
 
 
+
+
+def make_tool_button(frame, height, width, x, y, command, image_dir):
+    image = Image.open(image_dir)
+    image = image.resize((45, 45), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(image)
+
+    new_button = tk.Button(frame, height=height, width=width, bg=TOOL_BG, image=image)
+    new_button.config(activebackground=TOOL_BG_CLICKED, command=command)
+    new_button['borderwidth'] = 0.5
+    new_button.image = image
+    new_button.place(x=x, y=y, anchor="nw")
+    new_button.bind("<Enter>", mouse_enter_tool)
+    new_button.bind("<ButtonRelease>", mouse_release_tool)
+    new_button.bind("<Leave>", mouse_leave_tool)
+    return new_button
+
+
