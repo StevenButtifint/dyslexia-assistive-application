88453b7159cf549a2ccd021fabd5d3b193191877
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-23 10:26:22 +0000

    added tool colour constants

--- res/constants.py
+++ res/constants.py
@@ -7,5 +7,11 @@ HOME_DIMENSIONS = "600x75"
 APP_ICON_DIR = "res/icons/ico_48.ico"
 HOME_ICON_DIR = "res/icons/home_48.png"
 
+# Colours
+TOOLBAR_BG = "#1c5a8a"
+TOOL_BG = "#0a98cc"
+TOOL_BG_HOVER = "#4ad2ff"
+TOOL_BG_CLICKED = "#4ad2fe"
+
 # labels
 HOME_NOTICE = "Welcome! Select a tool to get started."
