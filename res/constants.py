3c2f78155c1152d2e30dcbbe5f2b2398633e095b
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-23 10:25:52 +0000

    added home constants

--- res/constants.py
+++ res/constants.py
@@ -5,3 +5,7 @@ HOME_DIMENSIONS = "600x75"
 
 # Directories
 APP_ICON_DIR = "res/icons/ico_48.ico"
+HOME_ICON_DIR = "res/icons/home_48.png"
+
+# labels
+HOME_NOTICE = "Welcome! Select a tool to get started."
