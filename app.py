9299285a956d510f2a2da7648d27c321a15a8842
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-23 10:28:32 +0000

    added tool selected method

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ class Window:
     def _create_tool_bar(self):
         toolbar_frame = make_static_frame(self.window, TOOLBAR_BG, 2000, 85, 0, 0, "nw")
 
+    def _tool_selected(self, tool_button):
+        self.window.attributes('-alpha', 1)
+        if self.tool_selected is not tool_button:
+            self.tool_selected.config(bg=TOOL_BG)
+        self.tool_selected = tool_button
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
