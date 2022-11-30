f76ed4813b711f6bf08640e74afe03d059e1455c
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-30 16:47:45 +0000

    added colour constants

--- res/constants.py
+++ res/constants.py
@@ -12,6 +12,13 @@ TOOLBAR_BG = "#1c5a8a"
 TOOL_BG = "#0a98cc"
 TOOL_BG_HOVER = "#4ad2ff"
 TOOL_BG_CLICKED = "#4ad2fe"
+MAIN_BG = "#59a4bd"
+ENTRY_BG = "#8fbfcf"
+BUTTON_BG = "purple"
+BUTTON_BG_HOVER = "#4ad2ff"
+NOTICE_BG = "#4ad2fe"
+NOTICE_TEXT = "black"
+
 
 # labels
 HOME_NOTICE = "Welcome! Select a tool to get started."
