3ff9f22a098eb8c7b0fcc6e298f85bfbae878956
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-12-27 18:45:56 +0000

    updated imports

--- res/interface.py
+++ res/interface.py
@@ -1,5 +1,12 @@
+import os
 import tkinter as tk
 import tkinter.font as font
+
+from PIL import Image, ImageTk
+
+from res.constants import *
+
+
 def make_static_frame(frame, bg, w, h, x, y, anchor):
     new_frame = tk.Frame(frame, bg=bg)
     new_frame.place(width=w, height=h, x=x, y=y, anchor=anchor)
