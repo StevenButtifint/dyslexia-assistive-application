e0327dcb4388ea5cf79a10140f146be14e6683dd
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-01-19 18:07:48 +0000

    added text sanitisation and extraction from input textbox

--- app.py
+++ app.py
@@ -167,6 +167,12 @@ class Window:
         output_label.config(font=font.Font(slant="italic"))
 
         make_button(self.options_frame, "Check Spelling", 1, 13, MAIN_BG, "black", 295, 540, lambda: self._check_spelling(result_listbox, self.text_box.get("1.0", tk.END)), 14)
+
+    def _check_spelling(self, result_listbox, raw_string):
+        no_punctuation = raw_string.translate(str.maketrans('', '', string.punctuation))
+        word_set = no_punctuation.split()
+        with open(self.resource_path(ENG_DICT_DIR)) as f:
+            dict_words = f.read().splitlines()
     def _text_speaker(self):
         self._tool_selected(self.text_reader)
         self.notice_label.config(text=TEXT_SPEAKER_NOTICE)
