df8b42aa0721a4e64487a1fbe68eeb4ece8a2aea
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-13 14:16:38 +0000

    updated button constants

--- res/constants.py
+++ res/constants.py
@@ -1,4 +1,3 @@
-
 # GUI
 APP_TITLE = "Dyslexia Assistive Application"
 HOME_DIMENSIONS = "597x75"
@@ -33,7 +32,7 @@ TOOL_BG_HOVER = "#4ad2ff"
 TOOL_BG_CLICKED = "#4ad2fe"
 MAIN_BG = "#59a4bd"
 ENTRY_BG = "#8fbfcf"
-BUTTON_BG = "purple"
+BUTTON_BG = "#79c4dd"
 BUTTON_BG_HOVER = "#4ad2ff"
 NOTICE_BG = "#4ad2fe"
 NOTICE_TEXT = "black"
